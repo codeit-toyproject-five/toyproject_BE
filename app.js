cefa0fbd99fdade88d265860b7dd3e242f97c8c3
gunapark <ncc0816@gmail.com>
2024-10-05 14:38:47 +0900

    그룹 수정 passowrd확인

--- app.js
+++ app.js
@@ -1316,6 +1316,10 @@ app.patch('/api/groups/:groupId',asyncHandler(async (req,res)=>{
   if(!group){
     return res.status(404).send({message : "존재하지 않습니다"});
   }
+  console.log("Received password from client:", req.body.password);
+  console.log("type of received password: ", typeof req.body.password);
+  console.log("Stored password in database:", group.password);
+  console.log("stored password in Db: ", typeof group.password);
   if(group.password === req.body.password){
     group.name = req.body.name;
     group.imageUrl = req.body.imageUrl;
